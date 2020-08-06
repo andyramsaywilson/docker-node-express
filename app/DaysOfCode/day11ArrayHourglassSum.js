b3ddb4331903b65268a49f49bc89ab7613f547ae
Andy Ramsay-Wilson <andy@iterative-consulting.co.uk>
2020-08-06 15:05:35 +0100

    Day 11 - Array Hourglass - class refactor

--- app/DaysOfCode/day11ArrayHourglassSum.js
+++ app/DaysOfCode/day11ArrayHourglassSum.js
@@ -1,4 +1,4 @@
-module.exports = class Day11ArrayHourglassSum {
+class Hourglass {
 	constructor() {
 		this.hourglass = [
 			[0, 0],
@@ -10,33 +10,49 @@ module.exports = class Day11ArrayHourglassSum {
 			[2, 2],
 		];
 
-		this.hourglassX = 0;
-		this.hourglassY = 1;
-		this.hourglassSize = 3;
+		this.x = 0;
+		this.y = 1;
+		this.size = 3;
 	}
 
-	calculate (arr) {
-		const furthestStart = arr.length - this.hourglassSize;
-
-		let highestTotal = null;
-		for (var x = 0; x <= furthestStart; x++) {
-			for (var y = 0; y <= furthestStart; y++) {
-				let total = this.calculateHourglass(arr, x, y);
-				highestTotal = (highestTotal === null || highestTotal < total) ? total : highestTotal;
-			}
+	calculateForMatrix(matrix, startX, startY) {
+		let matrixSize = matrix.length;
+		if (matrixSize - startX < this.size || matrixSize - startY < this.size) {
+			return null;
 		}
-		return highestTotal;
-	}
 
-	calculateHourglass(matrix, startX, startY) {
 		let total = 0;
-		let x = this.hourglassX;
-		let y = this.hourglassY;
+
+		let x = this.x;
+		let y = this.y;
+
 		this.hourglass.forEach(function(relative) {
 			let absoluteX = startX + relative[x];
 			let absoluteY = startY + relative[y];
 			total += matrix[absoluteY][absoluteX];
 		});
 		return total;
+	}
+}
+
+module.exports = class Day11ArrayHourglassSum {
+	constructor() {
+		this.hourglass = new Hourglass();
+	}
+
+	calculate (arr) {
+
+		let highestTotal = null;
+
+		for (let x = 0; x < arr.length; x++) {
+			for (let y = 0; y <= arr.length; y++) {
+				let total = this.hourglass.calculateForMatrix(arr, x, y);
+				if (total === null) {
+					continue;
+				}
+				highestTotal = (highestTotal === null || highestTotal < total) ? total : highestTotal;
+			}
+		}
+		return highestTotal;
 	}
 }
